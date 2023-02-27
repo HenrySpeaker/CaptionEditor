94a92b1cfcd3b6f59e800e4af72da3c63f9bfacb
Henry Speaker <73503466+HenrySpeaker@users.noreply.github.com>
2023-02-27 01:38:09 -0500

    Begin constructing tests and create the pyproject.toml file

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,1 +1,7 @@
 import pytest
+from src.converter import Converter
+
+
+def test_captions_not_found():
+    with pytest.raises(FileNotFoundError):
+        converter = Converter("test.vtt", "test.txt")
